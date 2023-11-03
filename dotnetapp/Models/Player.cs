b3cffe903a1c8768c20e6cddbf5e4ded28055a9c
Iamneo <techadmin@iamneo.ai>
2023-11-03 12:20:40 +0000

    Backup Commit

--- dotnetapp/Models/Player.cs
+++ dotnetapp/Models/Player.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 namespace dotnetapp.Models
 {
     public class Player
-    {
+    {public int Id{get;set;}
+    public int Age{get;set;}
+    public string Name{get;set;}
+    public decimal BiddingPrice{get;set;}
         // Write your Player class here...        
     }
 }
